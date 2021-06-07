7074e3b2dfc2f434c69abcd330ba0f8558b23033
mr-rampage <readonly@che.wbac.ca>
2021-06-07 18:39:31 -0400

    :memo: Add documentation

--- Chapter 1.js
+++ Chapter 1.js
@@ -1,8 +1,19 @@
 const test = require('tape');
 const fc = require('fast-check');
 
+/**
+ * Identity
+ * @param {*} x - anything
+ * @return {*} x
+ */
 const id = x => x;
-const compose = (...f) => x => f.reverse().reduce((i, f) => f(i), x);
+
+/**
+ * Compose - combines all functions from right to left
+ * @param {...function(*): *} f - a list of single argument function
+ * @return {function(*): *} - a single argument function
+ */
+const compose = (...f) => x => f.reverse().reduce((x, f) => f(x), x);
 
 test('identity', assert => {
   assert.plan(1);
@@ -10,7 +21,7 @@ test('identity', assert => {
     fc.assert(
       fc.property(fc.anything(), x => x === id(x))
     )
-  }, "should return the input for all inputs")
+  }, 'should be universally polymorphic')
 });
 
 test('composition', assert => {
@@ -19,23 +30,23 @@ test('composition', assert => {
 
   assert.plan(3);
   assert.doesNotThrow(() => {
-      fc.assert(
-        fc.property(fc.integer(), fc.integer(), (x, y) =>
-          compose(adder(x), multiplier(y))(x) === adder(x)(multiplier(y)(x)))
-      )
-  }, "should compose from right to left");
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (x, y) =>
+        compose(adder(x), multiplier(y))(x) === adder(x)(multiplier(y)(x)))
+    )
+  }, 'should compose from right to left');
 
   assert.doesNotThrow(() => {
     fc.assert(
       fc.property(fc.integer(), fc.integer(), (x, y) =>
         compose(adder(x), multiplier(y), adder(y))(x) === compose(compose(adder(x), multiplier(y)), adder(y))(x))
     )
-  }, "should be associative")
+  }, 'should be associative')
 
   assert.doesNotThrow(() => {
     fc.assert(
       fc.property(fc.integer(), fc.integer(), (x, y) =>
         compose(adder(x), id)(y) === compose(id, adder(x))(y))
     )
-  }, "should contain a unit of composition")
+  }, 'should contain a unit of composition')
 })
